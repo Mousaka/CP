ec4314d0d27ae419c7eccaa11169684d729dbe22
Kristian Lundström <kristian.lundstrom@gmail.com>
2014-11-11 22:08:33 +0100

    Finds R for Irish coffee so that  it cools quicker when added whiskey first

--- coffee.f
+++ coffee.f
@@ -1,28 +1,36 @@
       program cooling
       implicit none
       real r,time,t_max,T,Ts,T_ini,delta_t, getR
+      real r_irish, r_black, time1, time2
+      real time_diff
       integer pgopen,i,n_steps,i_again, addedMilk, zipAble
-
- 100  write(*,*) "Cooling rate and surroundind temperature = 17:"
-      read(*,*) r  !, Ts
+      integer black_greater
+      if(pgopen('/xwin').le.0) stop
+      call pgpap (8.0,0.8)
+      call pgask(.false.)
+ 100  write(*,*) "Enter cooling rate coffee, temperature = 17:"
+      read(*,*) r_black  !, Ts
+      write(*,*) "Enter irish cooling rate guess for it to be" 
+      write(*,*) "more effective to add it first"
+      read(*,*) r_irish
       Ts = 17
       write(*,*)     'Time duration and initial temperature:'
       !read(*,*) t_max, T_ini
       t_max = 20
       T_ini = 90.0
-      
-      if(pgopen('/xwin').le.0) stop
-      call pgpap (5.0,0.75)
-
  102  write (*,*) 'Please type in the small time interval delta_t :'
       delta_t = 0.001 !read (*,*) delta_t
-      time = 0
-      T = T_ini
+ 103  time = 0
+      T = T_ini      
       call pgeras
       call pgenv(0.0,t_max,70.0,T_ini+5,0,0)
       call pglab('Time (min)','Temperature (C)','Cooling Time')
+
+      black_greater = 1
+      if(r_black.le.r_irish) black_greater = -1
       !call readData()  not needed now
       !adding milk first
+      r=r_black
       zipAble = 0
       n_steps = int(t_max/delta_t)
       call pgsci(2)
@@ -31,13 +39,15 @@
         if (zipAble.eq.0.and.T.le.75.0) then
           call pgsci(3)
           zipAble = 1
-          write(*,*) "Milk at start makes coffee sipable after ",time
+          !write(*,*) "Whiskey at start makes coffee sipable after ",time
+          time1 = time
         end if
         !write(*,*) "Plot plot"
         call pgpt (1,time,T,-1)
         if (i.eq.2) then
           call pgmove(time, T)
           T = T - 5
+          r = r_irish
           call pgsci(4)
           call pgdraw(time, T)
           call pgsci(2)
@@ -49,6 +59,7 @@
       time = 0
       zipAble = 0
       n_steps = int(t_max/delta_t)
+      r = r_black
       T = T_ini
       call pgsci(6)
       do i=1,n_steps
@@ -57,31 +68,51 @@
           call pgmove(time,T)
           addedMilk = 1
           T = T-5
+          r = r_irish
           call pgsci(1)
           call pgdraw(time, T)
           call pgsci(6)
         end if
         if (zipAble.eq.0.and.T.le.75.0) then
           call pgsci(7)
-          write(*,*) "Milk at end makes coffee sipable after ", time
+          !write(*,*) "Milk at end makes coffee sipable after ", time
+          time2 = time
           zipAble = 1
         end if
         !write(*,*) "Plot plot"
         call pgpt (1,time,T,-1)
       end do
       call pgsci(1)
-
+      time_diff = time1 - time2
+      if(abs(time_diff).le.0.1) then
+        write(*,*) "Adding whiskey first better when r_irish=", r_irish
+      else
+        if(time1.le.time2) then
+          r_irish = r_irish - 0.0001
+        else
+          r_irish = r_irish + 0.0001
+        end if
+        call sleep_time(2)
+        goto 103
+      end if
       write (*,*) 'Again ? (1=YES;0=NO)'
       read (*,*) i_again
       if (i_again.eq.1) goto 100
       call pgclos
 
-      end
+      end program
 
       subroutine Euler(t, T_coffee, T_room, r, delta_t)
         change = -r*(T_coffee - T_room)*delta_t
         T_coffee = T_coffee + change
         t = t + delta_t
+      end
+
+      subroutine sleep_time(st)
+      integer st
+      do i = 1, (10000000 * st)
+       idummy = i**3
+      enddo
       end
 
       subroutine readData()
@@ -101,9 +132,4 @@
         call pgline(23, time_stamps, temps_black)
         call pgpt(23, time_stamps, temps_black, 2)
         return
-      end
-
-      function getR(t_stamps, temps_coffee)
-      real temps_coffee(23)
-      getR = 1
       end
