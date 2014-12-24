ad4ab4f5715c7e5d2094c4b54548e968c54d50ec
Kristian Lundström <kristian.lundstrom@gmail.com>
2014-12-24 08:19:41 +0100

    freefall done i think. Improved graph a bit

--- free_fall.f
+++ free_fall.f
@@ -3,11 +3,13 @@
 
       real radius, g, h, delta_h, v1, v2
       real percentage
+      character(len=50) :: string
       integer pgopen, i_again
       if (pgopen('/xwin').le.0) stop
       call pgpap(5.0, 1.0)
       call pgeras
       call pgenv(0.0,100000.0,0.0,1300.0,0.0,0)
+      call pglab('Height (m)','Velocity (m/s**2)','Method comparison')
       delta_h = 0.1
       v1 = 0
       v2 = 0
@@ -16,13 +18,15 @@
      	h = h + delta_h
       	call first_line(v1, h, delta_h)
       	call second_line(v2, h,  delta_h)
-c      	write(*,*) "h, v:",h, v1
+c      	write(*,*) "h, vX§:",h, v1
 c      	write(*,*) "h, v:", h, v
 		percentage = ((v1-v2) / v1) * 100
 c		write(*,*) "percentage: " , percentage
 		if(percentage.le.-1) then
 			write(*,*) "Difference is 1% when h = ", h
-	  call pgptext(h + 1000,v1,0.0,0.0,"1 percent difference")
+			write(string, *) "h=", h, "m"
+	  call pgptext(6000,v1,0.0,0.0,"1 percent difference when")
+	  call pgptext(6000,v1-100,0.0,0.0, string)
 			call pgmove(h, 1200)
 			call pgdraw(h, 1)
 			goto 100
